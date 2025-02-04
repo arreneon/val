893aba0eebbb6475e89d37e66cd184022f9a8f47
arreneon <arreneonx@gmail.com>
2025-02-04 23:31:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "elys please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, say yes please! "
 ];
 
 let messageIndex = 0;
